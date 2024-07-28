76969297581d2ee1ec0656bbd56312bb5a4e75d5
Radik Friedrichs <radik.fr@proton.me>
2024-07-28 02:18:52 +0200

    added minestom

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,5 +13,5 @@ repositories {
 dependencies {
     implementation("log4j:log4j:1.2.17")
 
-    compileOnly("net.minestom:minestom-snapshots:12794d4263")
+    implementation("net.minestom:minestom:12794d4263")
 }
