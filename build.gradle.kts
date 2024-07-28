05db43b51fef52fa4c047d3d5bd01cf33aec1984
Radik Friedrichs <radik.fr@proton.me>
2024-07-28 02:29:09 +0200

    stable minestom release

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,13 +8,13 @@ version = "1.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
-    maven(url = "https://jitpack.io")
+    maven(url = "https://s01.oss.sonatype.org/content/repositories/snapshots/")
 }
 
 dependencies {
     implementation("log4j:log4j:1.2.17")
 
-    compileOnly("com.github.Minestom:minestom:461d0aa746")
+    compileOnly("net.minestom:minestom-snapshots:1f34e60ea6")
 }
 
 tasks.withType<JavaCompile> {
